876dd04b4ccd2dc7a1b22819d2f7aec409f0dd76
Dorothy Ng <dng1@stuy.edu>
2016-11-09 20:48:14 -0500

    Create main.c

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <signal.h>
+
+int main(){
+  return 0;
+}
